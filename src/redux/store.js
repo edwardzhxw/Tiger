bbbfedfc9fc8275ca3466f04c4695f8663b53967
Edward <edward.zhang.xw@gmail.com>
2016-07-26 11:34:23 +0800

    Refactor Navigation

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,17 @@ import * as reduxLoop from 'redux-loop';
 import middleware from './middleware';
 import reducer from './reducer';
 
-const enhancer = compose(
+let enhancers = [
   applyMiddleware(...middleware),
   reduxLoop.install()
-);
+];
+
+if (__DEV__) {
+  let devTools = require('remote-redux-devtools');
+  enhancers = [...enhancers, devTools()];
+}
+
+const enhancer = compose(...enhancers);
 
 // create the store
 const store = createStore(
